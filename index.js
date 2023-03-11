d405a65d4d7496e5ed49984f4a8815d87ae8d628
Marvyn Kiplagat <marvynrogony@gmail.com>
2023-03-11 14:06:52 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+function writeCards(names) {
+    const messages = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      const message = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+      messages.push(message);
+    }
+  
+    return messages;
+  }
+  
+
+  function countDown(num){
+    let i = num;
+    while(i >= 0){
+        console.log(i);
+        i--;
+    }
+  }
+
+  countDown();
